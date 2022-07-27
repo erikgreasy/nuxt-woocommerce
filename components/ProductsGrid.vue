2216449149fbaaa005462a20c40611cfc78afe06
erikgreasy <erik.masny@gmail.com>
2022-07-27 21:54:17 +0200

    feat: show grid of fetched product on HP

---
+++ components/ProductsGrid.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="grid grid-cols-3 gap-10">
+        <article v-for="product in products" :key="product.id" class="text-center bg-gray-100 p-10">
+            <h3>
+                <NuxtLink :to="`/products/${product.id}`">{{ product.name }}</NuxtLink>
+            </h3>
+        </article>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            products: []
+        }
+    },
+
+    methods: {
+        async getProducts() {
+            const res = await this.$axios.get('products')
+            this.products = res.data
+        }
+    },
+
+    mounted() {
+        this.getProducts()
+    }
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <Tutorial/>
+  <div class="container mx-auto px-4">
+    <header>
+      <h1>Welcome to our shop</h1>
+    </header>
+    <div>
+      <h2 class="text-2xl text-center">Produkty</h2>
+      <ProductsGrid />
+    </div>
+  </div>
 </template>
 
 <script>
