0bb9029c5d09111c41f81851aae472fe03c45fe3
erikgreasy <erik.masny@gmail.com>
2022-07-27 22:58:49 +0200

    feat: create default layout with header

---
+++ components/AppHeader.vue
@@ -0,0 +1,19 @@
+<template>
+    <header>
+        <div class="container flex justify-between items-center">
+            <h1 class="text-3xl font-semibold">
+                <NuxtLink to="/">NUXTCOMMERCE</NuxtLink>
+            </h1>
+            <nav>
+                <ul class="flex gap-x-5">
+                    <li>
+                        <NuxtLink to="/">Home</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/checkout">Checkout</NuxtLink>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</template>
