fb74403ffbd93f7b0a3ed680a7a63d8254b58644
erikgreasy <erik.masny@gmail.com>
2022-08-08 23:38:31 +0200

    feat: style products grid

---
+++ components/ProductCard.vue
@@ -0,0 +1,23 @@
+<template>
+    <article>
+        <NuxtLink :to="`/products/${product.id}-${product.slug}`">
+            <img v-if="product?.images?.length" :src="product.images[0].src" :alt="product.images[0].alt" class="rounded-lg h-[400px] w-full object-cover transition hover:opacity-80">
+        </NuxtLink>
+        <h3 class="mt-2 mb-2">
+            <NuxtLink :to="`/products/${product.id}-${product.slug}`">{{ product.name }}</NuxtLink>
+        </h3>
+        <div class="text-lg" v-html="product.price_html"></div>
+    </article>
+</template>
+
+<script>
+export default {
+    props: {
+        product: Object,
+    }
+}
+</script>
+
+<style>
+
+</style>
--- components/ProductsGrid.vue
+++ components/ProductsGrid.vue
@@ -1,20 +1,22 @@
 <template>
     <div>
         <div class="grid grid-cols-3 gap-10" v-if="$fetchState.pending">
-            <article v-for="index in 6" :key="index" class="w-full h-28 bg-gray-200 animate-pulse"></article>
+            <div v-for="index in 6" :key="index" class="w-full h-[400px] bg-gray-200 animate-pulse"></div>
         </div>
         <div v-else class="grid grid-cols-3 gap-10">
-            <article v-for="product in products" :key="product.id" class="text-center bg-gray-100 p-10">
-                <h3>
-                    <NuxtLink :to="`/products/${product.id}-${product.slug}`">{{ product.name }}</NuxtLink>
-                </h3>
-            </article>
+            <ProductCard v-for="product in products" :key="product.id" :product="product" />
         </div>
     </div>
 </template>
 
 <script>
+import ProductCard from './ProductCard.vue'
+
 export default {
+    components: {
+        ProductCard,
+    },
+
     data() {
         return {
             products: []
